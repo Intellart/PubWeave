b0f57b2be2f8eb7b62f4b9961bb66b3db5950202
Mateo <mateo1559@gmail.com>
2022-12-14 16:19:13 +0100

    wip

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,59 @@
-import React from 'react';
-
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+import EditorJS from '@editorjs/editorjs';
 import { createReactEditorJS } from 'react-editor-js';
+import { isEmpty } from 'lodash';
+
+import * as API from '../../api';
 
 import { EDITOR_JS_TOOLS } from './constants';
 
 import logoImg from '../../images/LogoPubWeave.png';
 import 'bulma/css/bulma.min.css';
 
-const ReactEditorJS = createReactEditorJS();
+// const ReactEditorJS = createReactEditorJS();
 
 function ReactEditor () {
+  const editorRef = React.useRef(null);
+  const [defaultValue, setDefaultValue] = React.useState(null);
+
+  async function fetchArticle () {
+    if (isEmpty(defaultValue)) {
+      const response = await API.getRequest('blog_articles/2');
+      setDefaultValue(response.article_content);
+      console.log('response', response);
+    }
+  }
+
+  fetchArticle();
+
+  useEffect(() => {
+    if (!isEmpty(defaultValue)) {
+      const editor = new EditorJS({
+        holder: 'editorjs',
+        data: {
+          blocks: defaultValue.blocks,
+        },
+        tools: EDITOR_JS_TOOLS,
+        /**
+          * onChange callback
+          */
+        onChange: (api, event) => {
+          // console.log('Now I know that Editor\'s content changed!', event);
+
+          api.saver.save().then((articleContent) => {
+            console.log('savedData', articleContent);
+
+            API.putRequest('blog_articles/2', { blog_article: { article_content: articleContent } });
+          });
+        },
+        autofocus: true,
+        placeholder: 'Let`s write an awesome story!',
+      });
+    }
+  }, [editorRef, defaultValue]);
+
   return (
     <main className="home-wrapper">
       <nav className='navbar'>
@@ -33,135 +77,7 @@ function ReactEditor () {
           <button className='submit-work'>Submit your research</button>
         </div>
       </nav>
-
-      <section className="text-editor-section">
-        <ReactEditorJS
-          tools={EDITOR_JS_TOOLS}
-          defaultValue={{
-            time: 1635603431943,
-            blocks: [
-              {
-                id: 'sheNwCUP5A',
-                type: 'header',
-                data: {
-                  text: 'Editor.js',
-                  level: 2,
-                },
-              },
-              {
-                id: '12iM3lqzcm',
-                type: 'paragraph',
-                data: {
-                  text:
-                  'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text.',
-                },
-              },
-              {
-                id: 'fvZGuFXHmK',
-                type: 'header',
-                data: {
-                  text: 'Key features',
-                  level: 3,
-                },
-              },
-              {
-                id: 'xnPuiC9Z8M',
-                type: 'list',
-                data: {
-                  style: 'unordered',
-                  items: [
-                    'It is a block-styled editor',
-                    'It returns clean data output in JSON',
-                    'Designed to be extendable and pluggable with a simple API',
-                  ],
-                },
-              },
-              {
-                id: '-MhwnSs3Dw',
-                type: 'header',
-                data: {
-                  text: 'What does it mean «block-styled editor»',
-                  level: 3,
-                },
-              },
-              {
-                id: 'Ptb9oEioJn',
-                type: 'paragraph',
-                data: {
-                  text:
-                  'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class="cdx-marker">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.',
-                },
-              },
-              {
-                id: '-J7nt-Ksnw',
-                type: 'paragraph',
-                data: {
-                  text:
-                  'There are dozens of <a href="https://github.com/editor-js">ready-to-use Blocks</a> and the <a href="https://editorjs.io/creating-a-block-tool">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games.',
-                },
-              },
-              {
-                id: 'SzwhuyoFq6',
-                type: 'header',
-                data: {
-                  text: 'What does it mean clean data output',
-                  level: 3,
-                },
-              },
-              {
-                id: 'x_p-xddPzV',
-                type: 'paragraph',
-                data: {
-                  text:
-                  'Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below',
-                },
-              },
-              {
-                id: '6W5e6lkub-',
-                type: 'paragraph',
-                data: {
-                  text:
-                  'Given data can be used as you want: render with HTML for <code class="inline-code">Web clients</code>, render natively for <code class="inline-code">mobile apps</code>, create markup for <code class="inline-code">Facebook Instant Articles</code> or <code class="inline-code">Google AMP</code>, generate an <code class="inline-code">audio version</code> and so on.',
-                },
-              },
-              {
-                id: 'eD2kuEfvgm',
-                type: 'paragraph',
-                data: {
-                  text:
-                  'Clean data is useful to sanitize, validate and process on the backend.',
-                },
-              },
-              {
-                id: 'N8bOHTfUCN',
-                type: 'delimiter',
-                data: {},
-              },
-              {
-                id: 'IpKh1dMyC6',
-                type: 'paragraph',
-                data: {
-                  text:
-                  "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏",
-                },
-              },
-              {
-                id: 'FF1iyF3VwN',
-                type: 'image',
-                data: {
-                  file: {
-                    url: 'https://codex.so/public/app/img/external/codex2x.png',
-                  },
-                  caption: '',
-                  withBorder: false,
-                  stretched: false,
-                  withBackground: false,
-                },
-              },
-            ],
-          }}
-        />
-      </section>
+      <div ref={editorRef} id="editorjs" />
     </main>
   );
 }
--- src/components/pages/constants.js
+++ src/components/pages/constants.js
@@ -4,7 +4,7 @@ import List from '@editorjs/list';
 import Warning from '@editorjs/warning';
 import Code from '@editorjs/code';
 import LinkTool from '@editorjs/link';
-import Image from '@editorjs/image';
+// import Image from '@editorjs/image';
 import Raw from '@editorjs/raw';
 import Header from '@editorjs/header';
 import Quote from '@editorjs/quote';
@@ -12,24 +12,126 @@ import Marker from '@editorjs/marker';
 import CheckList from '@editorjs/checklist';
 import Delimiter from '@editorjs/delimiter';
 import InlineCode from '@editorjs/inline-code';
-import SimpleImage from '@editorjs/simple-image';
-import MathTex from 'editorjs-math';
+import SimpleImageApi from '@editorjs/simple-image';
+
+import MathTexApi from 'editorjs-math';
+import Tooltip from 'editorjs-tooltip';
+
+class MathTex extends MathTexApi {
+  constructor({ data, config, api }) {
+    super({ data, config, api });
+  }
+
+  enableEditing() {
+    super.enableEditing();
+    console.log('enableEditing');
+
+    // add button child to this.textNode
+    const button = document.createElement('button');
+    button.innerText = 'Click me';
+    button.addEventListener('click', () => {
+      console.log('button clicked');
+    });
+    this.textNode.appendChild(button);
+  }
+}
 
 export const EDITOR_JS_TOOLS = {
-  embed: Embed,
+  tooltip: {
+    class: Tooltip,
+    config: {
+      location: 'left',
+      highlightColor: '#FFEFD5',
+      underline: true,
+      backgroundColor: '#154360',
+      textColor: '#FDFEFE',
+      holder: 'editorId',
+    },
+  },
+  embed: {
+    class: Embed,
+    inlineToolbar: true,
+  },
   table: Table,
   marker: Marker,
   list: List,
+  math: {
+    class: MathTex,
+    inlineToolbar: true,
+  },
   warning: Warning,
   code: Code,
   linkTool: LinkTool,
-  image: Image,
+  image: {
+    class: Image,
+    inlineToolbar: ['link'],
+    config: {
+      /**
+       * Custom uploader
+       */
+      uploader: {
+        /**
+         * Upload file to the server and return an uploaded image data
+         * @param {File} file - file selected from the device or pasted by drag-n-drop
+         * @return {Promise.<{success, file: {url}}>}
+         */
+        uploadByFile(file) {
+          // your own uploading logic here
+          return MyAjax.upload(file).then(() => ({
+            success: 1,
+            file: {
+              url: 'https://codex.so/upload/redactor_images/o_80beea670e49f04931ce9e3b2122ac70.jpg',
+              // any other image data you want to store, such as width, height, color, extension, etc
+            },
+          }));
+        },
+
+        /**
+         * Send URL-string to the server. Backend should load image by this URL and return an uploaded image data
+         * @param {string} url - pasted image URL
+         * @return {Promise.<{success, file: {url}}>}
+         */
+        // eslint-disable-next-line no-unused-vars
+        uploadByUrl(url) {
+          console.log('uploadByUrl', url);
+
+          // your ajax request for uploading
+          return MyAjax.upload(file).then(() => ({
+            success: 1,
+            file: {
+              url: 'https://codex.so/upload/redactor_images/o_e48549d1855c7fc1807308dd14990126.jpg',
+              // any other image data you want to store, such as width, height, color, extension, etc
+            },
+          }));
+        },
+      },
+    },
+  },
+  simpleImage: {
+    class: SimpleImageApi,
+    inlineToolbar: ['link'],
+    config: {
+      uploader: {
+        uploadByURL(url) {
+          console.log('uploadByURL', url);
+
+          return new Promise((resolve) => {
+            resolve({
+              success: 1,
+              file: {
+                url,
+              },
+            });
+          });
+        },
+      },
+    },
+  },
   raw: Raw,
   header: Header,
   quote: Quote,
   checklist: CheckList,
   delimiter: Delimiter,
   inlineCode: InlineCode,
-  simpleImage: SimpleImage,
-  math: MathTex,
+
 };
