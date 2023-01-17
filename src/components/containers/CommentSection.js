c973f1e2e3e636f5b1c7a843459e9f62225f9568
Mateo <mateo1559@gmail.com>
2023-01-17 16:26:47 +0100

    mentions fixed

--- src/components/containers/CommentSection.js
+++ src/components/containers/CommentSection.js
@@ -1,12 +1,9 @@
-/* eslint-disable no-unused-vars */
-import { WindowSharp } from '@mui/icons-material';
-import {
-  Fade, Paper, Popover, Popper, TextareaAutosize, Typography,
-} from '@mui/material';
+import { Chip } from '@mui/material';
 import {
   get, includes, isEmpty, map,
 } from 'lodash';
 import React, { useCallback } from 'react';
+import { Mention, MentionsInput } from 'react-mentions';
 
 import Comment from './Comment';
 
@@ -15,10 +12,6 @@ function CommentSection() {
   const [expandedComment, setExpandedComment] = React.useState(null);
   const tempCommentRef = React.useRef(null);
   const [newCommentContent, setNewCommentContent] = React.useState('');
-  const [newCommentInFocus, setNewCommentInFocus] = React.useState(false);
-  const [mentionsPopoverActive, setMentionsPopoverActive] = React.useState({
-  });
-  const [currentCarretPosition, setCurrentCarretPosition] = React.useState(0);
 
   const handleReply = (replyContent, parentId) => {
     setTempComment({});
@@ -137,193 +130,108 @@ function CommentSection() {
     }
   };
 
-  const textAreaRef = React.useRef(null);
+  const users = [
+    {
+      id: 'isaac',
+      display: 'Isaac Newton',
+    },
+    {
+      id: 'sam',
+      display: 'Sam Victor',
+    },
+    {
+      id: 'emma',
+      display: 'emmanuel@nobody.com',
+    },
+  ];
 
-  function getCaretCharacterOffsetWithin(element) {
-    let caretOffset = 0;
-    const doc = element.ownerDocument || element.document;
-    const win = doc.defaultView || doc.parentWindow;
-    let sel;
-    if (typeof win.getSelection !== 'undefined') {
-      sel = win.getSelection();
-      if (sel.rangeCount > 0) {
-        const range = win.getSelection().getRangeAt(0);
-        const preCaretRange = range.cloneRange();
-        preCaretRange.selectNodeContents(element);
-        preCaretRange.setEnd(range.endContainer, range.endOffset);
-        caretOffset = preCaretRange.toString().length;
-      }
-    } else if ((sel === doc.selection) && sel.type !== 'Control') {
-      sel = doc.selection;
-      const textRange = sel.createRange();
-      const preCaretTextRange = doc.body.createTextRange();
-      preCaretTextRange.moveToElementText(element);
-      preCaretTextRange.setEndPoint('EndToEnd', textRange);
-      caretOffset = preCaretTextRange.text.length;
-    }
+  const mentionsInputStyle = {
+    control: {
+      backgroundColor: '#fff',
+      fontSize: 16,
+    // fontWeight: 'normal',
+    },
+    '&multiLine': {
+      control: {
+        fontFamily: 'monospace',
+        minHeight: 63,
+      },
+      highlighter: {
+        padding: 9,
+        border: '1px solid transparent',
+      },
+      input: {
+        padding: 9,
+        border: '1px solid silver',
+      },
+    },
+    '&singleLine': {
+      display: 'inline-block',
+      width: 180,
+      highlighter: {
+        padding: 1,
+        border: '2px inset transparent',
+      },
+      input: {
+        padding: 1,
+        border: '2px inset',
+      },
+    },
+    suggestions: {
+      list: {
+        backgroundColor: 'white',
+        border: '1px solid rgba(0,0,0,0.15)',
+        fontSize: 16,
+      },
+      item: {
+        padding: '5px 15px',
+        borderBottom: '1px solid rgba(0,0,0,0.15)',
+        '&focused': {
+          backgroundColor: '#cee4e5',
+        },
+      },
+    },
+  };
 
-    return caretOffset;
-  }
+  const mentionsStyle = {
+    backgroundColor: '#cee4e5',
+
+  };
 
   return (
     <div className="comment-section-wrapper">
       <div className="comment-section-new-comment-wrapper">
-        <textarea
-          ref={textAreaRef}
-          onFocus={() => setNewCommentInFocus(true)}
-          onBlur={() => setNewCommentInFocus(false)}
-          onSelect={(e) => {
-            const { selectionStart, selectionEnd } = e.target;
-            const { value } = e.target;
-            const selectedText = value.substring(selectionStart, selectionEnd);
-            const selectedTextLength = selectedText.length;
-
-            // console.log(selectedTextLength);
-            // console.log(selectedText);
-            // console.log(selectionStart);
-            // console.log(selectionEnd);
-
-            if (selectionStart === selectionEnd) {
-              const letter = newCommentContent[selectionStart - 1];
-
-              if (letter === '@') {
-                setCurrentCarretPosition(selectionStart);
-                console.log(selectionStart);
-                console.log(newCommentContent);
-
-                // finds all '\n' before selectionStart
-                const newLines = newCommentContent.substring(0, selectionStart).match(/\n/g);
-                const newLinesCount = newLines ? newLines.length : 0;
-                console.log('row', newLinesCount);
-
-                // find num of chars between last '\n' and selectionStart
-                let charsAfterLastNewLine = selectionStart - newCommentContent.lastIndexOf('\n') - 1;
-                if (charsAfterLastNewLine < 0) { charsAfterLastNewLine = selectionStart; }
-                console.log('col', charsAfterLastNewLine);
-
-                setMentionsPopoverActive({
-                  top: textAreaRef.current ? textAreaRef.current.offsetTop + 0.7 * window.innerHeight - newLinesCount * 20 : 0,
-                  left: textAreaRef.current ? textAreaRef.current.offsetLeft + charsAfterLastNewLine * window.getComputedStyle(textAreaRef.current).fontSize.replace('px', '') + 20 : 0,
-                });
-              } else {
-                setMentionsPopoverActive({});
-              }
-            }
-          }}
+        <MentionsInput
+          style={mentionsInputStyle}
           className="comment-section-new-comment"
           value={newCommentContent}
           onChange={(e) => setNewCommentContent(e.target.value)}
-        />
-        <Popover
-          hideBackdrop
-          disableAutoFocus
-          disableEnforceFocus
-          open={!isEmpty(mentionsPopoverActive)}
-          anchorReference="anchorPosition"
-          anchorPosition={{ top: mentionsPopoverActive.top || 0, left: mentionsPopoverActive.left || 0 }}
-          onClick={() => setMentionsPopoverActive({})}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
         >
-          <div className="comment-section-mentions-popover">
-            <div className="comment-section-mentions-popover-header">
-              <span className="comment-section-mentions-popover-header-title">People</span>
-              <span className="comment-section-mentions-popover-header-subtitle">Mention someone to notify them</span>
-            </div>
-            <div className="comment-section-mentions-popover-content">
+          <Mention
+            trigger="@"
+            displayTransform={(id) => `@${id}`}
+            style={mentionsStyle}
+            data={users}
+            renderSuggestion={({ display, id }, search, highlightedDisplay, index, focused) => (
               <div
-                onClick={() => {
-                  setNewCommentContent(newCommentContent.slice(0, currentCarretPosition) + 'johndoe ' + newCommentContent.slice(currentCarretPosition));
-                }}
-                className="comment-section-mentions-popover-content-item"
+                className={focused ? 'focused' : ''}
+                key={id}
+                style={{ backgroundColor: focused ? '#cee4e5' : 'transparent' }}
               >
-                <div className="comment-section-mentions-popover-content-item-avatar">
-                  <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="avatar" />
-                </div>
-                <div className="comment-section-mentions-popover-content-item-info">
-                  <span className="comment-section-mentions-popover-content-item-info-username">johndoe</span>
-                  <span className="comment-section-mentions-popover-content-item-info-name">John Doe</span>
-                </div>
+                {display} ({id})
+                <Chip
+                  sx={{
+                    ml: 1, height: 20, padding: 0, fontSize: 12,
+                  }}
+                  variant="outlined"
+                  label="Author"
+                  color='primary'
+                />
               </div>
-              <div
-                onClick={() => {
-                  setNewCommentContent(newCommentContent.slice(0, currentCarretPosition) + 'johndoe ' + newCommentContent.slice(currentCarretPosition));
-                }}
-                className="comment-section-mentions-popover-content-item"
-              >
-                <div className="comment-section-mentions-popover-content-item-avatar">
-                  <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="avatar" />
-                </div>
-                <div className="comment-section-mentions-popover-content-item-info">
-                  <span className="comment-section-mentions-popover-content-item-info-username">johndoe</span>
-                  <span className="comment-section-mentions-popover-content-item-info-name">John Doe</span>
-                </div>
-              </div>
-              <div
-                onClick={() => {
-                  setNewCommentContent(newCommentContent.slice(0, currentCarretPosition) + 'johndoe ' + newCommentContent.slice(currentCarretPosition));
-                }}
-                className="comment-section-mentions-popover-content-item"
-              >
-                <div className="comment-section-mentions-popover-content-item-avatar">
-                  <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="avatar" />
-                </div>
-                <div className="comment-section-mentions-popover-content-item-info">
-                  <span className="comment-section-mentions-popover-content-item-info-username">johndoe</span>
-                  <span className="comment-section-mentions-popover-content-item-info-name">John Doe</span>
-                </div>
-              </div>
-              <div
-                onClick={() => {
-                  setNewCommentContent(newCommentContent.slice(0, currentCarretPosition) + 'johndoe ' + newCommentContent.slice(currentCarretPosition));
-                }}
-                className="comment-section-mentions-popover-content-item"
-              >
-                <div className="comment-section-mentions-popover-content-item-avatar">
-                  <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="avatar" />
-                </div>
-                <div className="comment-section-mentions-popover-content-item-info">
-                  <span className="comment-section-mentions-popover-content-item-info-username">johndoe</span>
-                  <span className="comment-section-mentions-popover-content-item-info-name">John Doe</span>
-                </div>
-              </div>
-              <div
-                onClick={() => {
-                  setNewCommentContent(newCommentContent.slice(0, currentCarretPosition) + 'johndoe ' + newCommentContent.slice(currentCarretPosition));
-                }}
-                className="comment-section-mentions-popover-content-item"
-              >
-                <div className="comment-section-mentions-popover-content-item-avatar">
-                  <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="avatar" />
-                </div>
-                <div className="comment-section-mentions-popover-content-item-info">
-                  <span className="comment-section-mentions-popover-content-item-info-username">johndoe</span>
-                  <span className="comment-section-mentions-popover-content-item-info-name">John Doe</span>
-                </div>
-              </div>
-              <div
-                onClick={() => {
-                  setNewCommentContent(newCommentContent.slice(0, currentCarretPosition) + 'johndoe ' + newCommentContent.slice(currentCarretPosition));
-                }}
-                className="comment-section-mentions-popover-content-item"
-              >
-                <div className="comment-section-mentions-popover-content-item-avatar">
-                  <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="avatar" />
-                </div>
-                <div className="comment-section-mentions-popover-content-item-info">
-                  <span className="comment-section-mentions-popover-content-item-info-username">johndoe</span>
-                  <span className="comment-section-mentions-popover-content-item-info-name">John Doe</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Popover>
+            )}
+          />
+        </MentionsInput>
+
         <button type="button" className="comment-section-new-comment-button">
           Post
         </button>
